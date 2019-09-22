9e13edace17b248b7f4118e939e42c2911f462b1
Alena Gurina <veggurina@gmail.com>
2019-09-22 16:54:23 +0300

    Pep8 for HW1

--- HW1.py
+++ HW1.py
@@ -1,22 +1,34 @@
-#Calculator
-def calculator (x,s,y): #Create function with 3 arguments
-    d={'+','-','*','/'}
-    if s not in d:
-        a = "Mistake action"#Remove all action that function can`t do
-    else:
-        if s == '+':
-             a = x+y
-        if s == '-':
-            a = x-y
-        if s == '*':
-            a = x*y
-        if s == '/':
-            if y == 0:
-                a = "You can`t divide on zero"
+def calc():
+    while 0 == 0:
+        print("If you would like to exit press N")
+        exit_from_fun = input()
+        if exit_from_fun == "N":
+            break
+        else:
+            print("enter an integer, command (+,-,* or /) and 2nd integer")
+            x = int(input())
+            command = input()
+            y = int(input())
+            l_of_command = ['+', '-', '*', '/']
+            if command not in l_of_command:
+                print("Mistake action")
+                continue
             else:
-                a = x/y
-    return print(a)#get a result
-x =int(input()) #this part can also be in function
-s =input()# Но я не понимаю как сделать чтобы функция запрашивала данные с клавиатуры, а не вводить их в скобках
-y =int(input())
-calculator(x,s,y)
+                result = None
+                if command == '+':
+                    result = x + y
+                if command == '-':
+                    result = x - y
+                if command == '*':
+                    result = x * y
+                if command == '/':
+                    if y == 0:
+                        result = "You can`t divide on zero"
+                    else:
+                        result = x / y
+                print(result)
+                continue
+    return
+
+
+calc()
